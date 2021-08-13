bf3f9993a3a6406cbae96dbc62761f8a67afa0a3
James Darmody <jdarmody@developmentreddinet.net>
2021-08-13 13:07:35 -0700

    init

---
+++ src/modules/ng2-completer.module.d.ts
@@ -0,0 +1,5 @@
+import { ModuleWithProviders } from "@angular/core";
+export declare class Ng2CompleterModule {
+    static forRoot(): ModuleWithProviders<any>;
+    static forChild(): ModuleWithProviders<any>;
+}
